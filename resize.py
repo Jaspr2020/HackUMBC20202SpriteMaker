a99dbb700ca8ae7cd05c2764e1d150700e87b895
Griffin Sober <griffin.sober@gmail.com>
2022-10-01 20:42:26 -0400

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,11 +1,15 @@
 from PIL import Image, ImageDraw
-im = Image.open("Ninja.jpg")
-im = im.resize((1024, 1024), resample=0) 
+im = Image.open("fantano.webp")
+dimension = 1000
+
+ 
+im = im.resize((dimension, dimension), resample=0) 
 pixels = im.load()
 draw = ImageDraw.Draw(im)
+size = int(input("Enter a sprite size: "))
 
-ratio = 64
-jump = int(ratio/4)
+jump = int(dimension/size)
+ratio = int(dimension/jump)
 for i in range(ratio):
     #print(i)
     for j in range(ratio):
@@ -18,7 +22,10 @@ for i in range(ratio):
                 rsum += pixels[(i * jump) + k,(j * jump) + l][0]
                 gsum += pixels[(i * jump) + k,(j * jump) + l][1]
                 bsum += pixels[(i * jump) + k,(j * jump) + l][2]
-        fill =(int(rsum/256), int(gsum/256), int(bsum/256))
+
+    
+
+        fill =(round(rsum/(jump * jump)), round(gsum/(jump * jump)), round(bsum/(jump * jump)))
         draw.rectangle((i * jump, j * jump, (i * jump) + jump, (j * jump) + jump), fill, outline=None)
 im.show()
 
